3663dcc66abddb8f17a973cba1f17326a113d6dc
Aviral <aviralsharma1243@gmail.com>
2023-01-01 16:04:29 +0530

    Initial Commit

---
+++ lib/model/todo.dart
@@ -0,0 +1,21 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false
+  });
+  static List<ToDo> todoList(){
+    return [
+      ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
+      ToDo(id: '02', todoText: 'Breakfast', isDone: true),
+      ToDo(id: '03', todoText: 'Check Mails',),
+      ToDo(id: '04', todoText: 'College',),
+      ToDo(id: '05', todoText: 'Watch Movie',),
+      ToDo(id: '06', todoText: 'Diet 2',),
+    ];
+  }
+}
